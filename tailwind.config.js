17b7ea839cff23e12dbdd26723527b9d589dda2d
cw <fromnowwon@gmail.com>
2024-07-01 15:28:31 +0900

    docs: theme 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"weather-primary": "#87CEEB",
+				"weather-secondary": "#4682B4",
+			},
+		},
+		fontFamily: {
+			Playwrite: ["Playwrite DE Grund, cursive", "system-ui", "sans-serif"],
+		},
+		container: {
+			padding: "2rem",
+			center: true,
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+		},
 	},
 	plugins: [],
 };
