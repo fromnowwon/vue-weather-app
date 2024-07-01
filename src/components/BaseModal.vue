ad8a62d29a4c589264f4cb557f40073b659adbe0
cw <fromnowwon@gmail.com>
2024-07-01 16:09:53 +0900

    style: modal style 적용

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,7 +1,10 @@
 <template>
 	<Teleport to="body">
-		<div v-show="modalActive">
-			<div>
+		<div
+			v-show="modalActive"
+			class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+		>
+			<div class="p-4 bg-white self-start mt-32 max-w-screen-md">
 				<slot />
 				<button @click="$emit('close-modal')">Close</button>
 			</div>
