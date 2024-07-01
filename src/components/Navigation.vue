32949193df46f5de80ff2a0174e3467b15cad1f4
cw <fromnowwon@gmail.com>
2024-07-01 16:13:40 +0900

    style: Navigation style  추가

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,22 @@
 <template>
-	<header>
-		<nav class="container flex flex-col items-center">
-			<div class="flex items-center gap-3">
-				<RouterLink :to="{ name: 'home' }">
-					<h1>Vue Weather App</h1>
-				</RouterLink>
+	<header class="sticky">
+		<nav
+			class="container flex flex-col items-center sm:flex-row items-center gap-4 text-white py-6"
+		>
+			<RouterLink :to="{ name: 'home' }">
 				<div class="flex items-center gap-3">
-					<i @click="toggleModal">info</i>
-					<i>add</i>
+					<h1>Vue Weather App</h1>
 				</div>
+			</RouterLink>
 
-				<BaseModal
-					:modalActive="modalActive"
-					@close-modal="toggleModal"
-				></BaseModal>
+			<div class="flex gap-3 flex-1 justify-end">
+				<i @click="toggleModal">info</i>
+				<i>add</i>
 			</div>
+
+			<BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+				<h2>About this app</h2>
+			</BaseModal>
 		</nav>
 	</header>
 </template>
